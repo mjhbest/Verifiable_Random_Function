0b1a41d7c6c0969361fc3d5f0af9c884ab3577d2
Jaehyeon Myung <mjhbest@kaist.ac.kr>
2020-02-13 15:56:09 +0900

    Keygen Input Change
    Participant 말고 ECVRF 주체의 id 를 받아와서 Key를 만들도록 수정함

--- VRF_Sortition.py
+++ VRF_Sortition.py
@@ -3,53 +3,55 @@ from Keys import Key
 import random
 import nacl.encoding
 
-class Sortition:
 
+class Sortition:
     currentKey = None
-    def __init__(self): #일단은 Num 제외
+
+    def __init__(self):  # 일단은 Num 제외
         self.participant = None
         self.data = None
         self.N = 1
         self.Key = None
         self.Proof = None
 
-    def pick_winner(self,participant,n):  ##return (winner_lst, proof_of_winner,key)
+    def pick_winner(self, participant, n):  ##return (winner_lst, proof_of_winner,key)
+    """Make random seed with using ECVRF & make winner"""
         self.update_sortition(participant, n)
         keys = self.createKey(self.data)
         print("SecretKey : {}".format(keys.SecretKey.formatting()))
         randseed = self.make_random_seed(keys.SecretKey.formatting()['key'])
-        return self.RandomlyPick(randseed,participant)
+        return self.RandomlyPick(randseed, participant)
 
-    def update_sortition(self,participant,num):
+    def update_sortition(self, participant, num):
         self.participant = participant
         self.data = self.serialize(participant)
         self.N = num
 
-    def createKey(self,data): #Key generation with elliptic curve(ECVRF 25519)
+    def createKey(self, data):  # Key generation with elliptic curve(ECVRF 25519)
         keySet = Key()
         keySet.create_secretKey(data)
         self.Key = keySet
         return keySet
 
-    def serialize(self,participant_list):
+    def serialize(self, participant_list):
 
-        return self.concatenate(participant_list).encode('ascii') #example이 string을 받아오니까 우선 split으로 행렬
+        return self.concatenate(participant_list).encode('ascii')  # example이 string을 받아오니까 우선 split으로 행렬
 
-    def concatenate(self,lst):
+    def concatenate(self, lst):
         s = lst[0]
         for p in lst[1:]:
-            s= s + p
+            s = s + p
         return s
 
     def make_random_seed(self, SK):
         hashed = self.ECVRF_hash(SK)
-        hashseed = int.from_bytes(hashed[0:32],'little')
+        hashseed = int.from_bytes(hashed[0:32], 'little')
         return hashseed
 
-    def ECVRF_hash(self,SK):
+    def ECVRF_hash(self, SK):
         self.Proof = ECVRF.prove(SK, self.data)
         hashedList = ECVRF.ecvrf_proof_to_hash(self.Proof)
-        self.Key.PublicKey = ECVRF._get_secret_scalar_and_public_key(SK)[1]
+        self.Key.PublicKey = ECVRF.getPubKey(SK)[1]
         return hashedList
 
     def RandomlyPick(self, seed, domain):  # domain 스트링에서 seed 받아서 랜덤 function
@@ -58,12 +60,8 @@ class Sortition:
 
     def verify_sortition(self):  ##return boolean of verifying
         vrf_val = False
-        if ECVRF.ecvrf_verify(self.Key.PublicKey, self.Proof,self.serial_data) == "VALID":
+        if ECVRF.ecvrf_verify(self.Key.PublicKey, self.Proof, self.serial_data) == "VALID":
             vrf_val = True
         key_val = self.Key.verify_secretKey()
 
         return vrf_val & key_val
-
-
-
-
